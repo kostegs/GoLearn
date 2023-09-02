3690c3ddf4cee9bc7023286d56e15bd3a30320d0
Kostegs <ksuslin@gmail.com>
2023-09-03 02:06:34 +0300

    Added Lesson3 - Functions

--- LearnWorkSpace/Lesson3/Functions.go
+++ LearnWorkSpace/Lesson3/Functions.go
@@ -5,5 +5,49 @@ import (
 )
 
 func Functions() {
-	fmt.Println("test Lesson3")
+	Greet();
+	PersonGreet("Vasya");
+	FioGreet("John", "Smith");
+
+	//
+	first, second := 1,2
+	Sum := Sum(first, second);
+	fmt.Printf("Sum(): Sum of %v and %v = %v\n", first, second, Sum);
+
+	//
+	Summa, Multiply := SumAndMultiply(first, second);
+	fmt.Printf("SumAndMultiply(): Sum = %v, Multiply = %v\n", Summa, Multiply);
+
+	//
+	_, Multiply64 := NamedSumAndMultiply(first, second);
+	fmt.Printf("NamedSumAndMultiply(): Multiply = %v\n", Multiply64);
+
 }
+
+func Greet(){
+	fmt.Println("Greet(): Hello guys!")
+}
+
+func PersonGreet(name string){
+	fmt.Printf("PersonGreet(): Zdarova, %s\n", name);
+}
+
+func FioGreet(name, surname string){
+	fmt.Printf("FioGreet(): Hi, %s%s!\n", name, surname);
+}
+
+func Sum(first, second int) int {
+	sum := first + second;
+	return sum;
+}
+
+func SumAndMultiply(first, second int)(int, int){
+	return first + second, first * second;	
+}
+
+func NamedSumAndMultiply(first, second int) (sum int64, multiply int64){
+	sum = int64(first + second);
+	multiply = int64(first * second);
+	return; // или return sum, multiply
+}
+
