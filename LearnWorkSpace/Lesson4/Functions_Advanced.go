89b0a26d83963970b801a3728849f93674054ba5
Kostegs <ksuslin@gmail.com>
2023-09-04 17:39:39 +0300

    Added closure example to Lesson4

--- LearnWorkSpace/Lesson4/Functions_Advanced.go
+++ LearnWorkSpace/Lesson4/Functions_Advanced.go
@@ -9,8 +9,8 @@ func Functions_Advanced() {
 	first, second := 5, 3
 
 	// Работа с функциями, как со значениями
-	var multiplier func(x, y int) int                        // объявление переменной с типом функции
-	multiplier = func(x, y int) int { return x * y }         // присваивание функции данной переменной
+	var multiplier func(x, y int) int                         // объявление переменной с типом функции
+	multiplier = func(x, y int) int { return x * y }          // присваивание функции данной переменной
 	fmt.Printf("multiplier: %v\n", multiplier(first, second)) // непосредственный вызов функции
 
 	divider := func(x, y int) int { return x / y }
@@ -30,23 +30,35 @@ func Functions_Advanced() {
 	fmt.Printf("subtractFunc: %v\n", calculate(first, second, subtractFunc))
 
 	// Возврат функции из функции
-	divideBy2 := createDivider(2);
-	divideBy10 := createDivider(10);
+	divideBy2 := createDivider(2)
+	divideBy10 := createDivider(10)
 
-	fmt.Println("Divide by 2:", divideBy2(100));
-	fmt.Println("Divide by 10:", divideBy10(100));
+	fmt.Println("Divide by 2:", divideBy2(100))
+	fmt.Println("Divide by 10:", divideBy10(100))
 
 	// Замыкания
-	dollar := 30;
+	dollar := 30
 
-	getDollarValue := func() int{
-		return dollar;
+	getDollarValue := func() int {
+		return dollar
 	}
 
-	fmt.Println("Dollar value1: ", getDollarValue());
+	fmt.Println("Dollar value1: ", getDollarValue())
 
-	dollar = 70;
-	fmt.Println("Dollar value2: ", getDollarValue());
+	dollar = 70
+	fmt.Println("Dollar value2: ", getDollarValue())
+
+	// Closure example
+	digitForTest := 10
+	closureFunc1 := TestingClosure(digitForTest)
+
+	fmt.Print("ClosureFunc1 (10): ")
+	closureFunc1()
+
+	digitForTest = 20
+
+	fmt.Print("ClosureFunc1 (20): ")
+	closureFunc1()
 
 }
 
@@ -54,10 +66,19 @@ func calculate(x, y int, action func(x, y int) int) int {
 	return action(x, y)
 }
 
-func createDivider(divider int /* 2 */) func(x int) int{
-	dividerFunc := func(x int) int{
+func createDivider(divider int /* 2 */) func(x int) int {
+	dividerFunc := func(x int) int {
 		return x / divider /* 2 */
 	}
 
-	return dividerFunc;
+	return dividerFunc
+}
+
+func TestingClosure(digit int) func() {
+
+	ClosureFunc := func() {
+		fmt.Println("Digit = ", digit)
+	}
+
+	return ClosureFunc
 }
